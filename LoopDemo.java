3cc3ec097003959b266204abe004f6047b551074
ashwithareddy45 <ashwithareddy4545@gmail.com>
2025-07-11 12:25:33 +0530

    Add files via upload

---
+++ LoopDemo.java
@@ -0,0 +1,14 @@
+class LoopDemo
+{
+public static void main(String args[])
+{
+int sum=0,avg;
+for(int i=0;i<=10;i++)
+{
+sum=sum+i;
+}
+System.out.println("Sum"+sum);
+avg=sum/10;
+System.out.println("Average"+avg);
+}
+}
